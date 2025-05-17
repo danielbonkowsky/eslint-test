1d16117455751c81604faf34164a0cda15a80b40
danielbonkowsky <danielbonkowsky@gmail.com>
2025-05-17 16:45:53 -0700

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      js,
+      "@stylistic/js": stylisticJs
+    },
+    extends: ["js/recommended"],
+    rules: {
+      curly: "error",
+      "@stylistic/js/semi": ["error", "always"],
+      "@stylistic/js/brace-style": "error",
+      "@stylistic/js/indent": ["error", 2, { "SwitchCase": 1 }],
+      "@stylistic/js/max-statements-per-line": ["error", { "max": 1 }]
+    }
+  }
+]);
